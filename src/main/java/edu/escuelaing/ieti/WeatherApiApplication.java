5a181a52366cf7f649808c55c746a1d2275d6f6d
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-28 18:39:15 -0500

    Source Code

---
+++ src/main/java/edu/escuelaing/ieti/WeatherApiApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.ieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiApplication.class, args);
+	}
+
+}
