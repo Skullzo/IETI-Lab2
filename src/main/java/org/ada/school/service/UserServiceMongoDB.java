a4e331865e65b3f38cc7ab58b91ff351d74b05c8
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-28 22:23:53 -0500

    Source Code

---
+++ src/main/java/org/ada/school/service/UserServiceMongoDB.java
@@ -0,0 +1,59 @@
+package org.ada.school.service;
+import java.util.List;
+import org.ada.school.dto.UserDto;
+import org.ada.school.model.User;
+import org.ada.school.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceMongoDB implements UserService {
+    private final UserRepository userRepository;
+   
+       public UserServiceMongoDB(@Autowired UserRepository userRepository )
+       {
+           this.userRepository = userRepository;
+       }
+   
+       @Override
+       public User create( User user )
+       {
+           return userRepository.save( user );
+       }
+   
+       @Override
+       public User findById( String id )
+       {
+           return userRepository.findById( id ).orElse( null );
+       }
+   
+       @Override
+       public List<User> all()
+       {
+           return null;
+       }
+   
+       @Override
+       public boolean deleteById( String id )
+       {
+           if ( userRepository.existsById( id ) )
+        {
+            userRepository.deleteById( id );
+            return true;
+        }
+        return false;
+       }
+   
+       @Override
+       public User update( UserDto userDto, String id )
+       {
+           if ( userRepository.existsById( id ) )
+        {
+            User user = userRepository.findById( id ).get();
+            user.update( userDto );
+            userRepository.save( user );
+            return user;
+        }
+        return null;
+       }
+}
