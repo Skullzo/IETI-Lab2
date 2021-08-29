3d01049811c962c1c37c76b3dab4d2e8506d4e6d
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-28 19:13:39 -0500

    Source Code

--- src/main/java/edu/escuelaing/ieti/repository/UserRepository.java
+++ src/main/java/edu/escuelaing/ieti/repository/UserRepository.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.escuelaing.ieti.repository;
-
-/**
- *
- * @author skull
- */
-public interface UserRepository {
-    
-}
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface UserRepository extends MongoRepository<UserDocument, String>
+  {}
