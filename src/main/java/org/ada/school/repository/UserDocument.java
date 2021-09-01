5ca6800eabb342765764ebb74a137dd6ecdd9425
Alejandro Toro Daza <skullzo@outlook.com>
2021-09-01 10:09:44 -0500

    Source Code

--- src/main/java/org/ada/school/repository/UserDocument.java
+++ src/main/java/org/ada/school/repository/UserDocument.java
@@ -16,9 +16,26 @@ public class UserDocument {
   
     String lastName;
 
-    String createdAt;
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
   
     public UserDocument() {
-        
+        name= userDto.getName();
+        email= userDto.getEmail();
+        lastName=userDto.getLastName();
+    }
+    public void update(UserDto userDto) {
+        name= userDto.getName();
+        email= userDto.getEmail();
+        lastName=userDto.getLastName();
     }
 }
