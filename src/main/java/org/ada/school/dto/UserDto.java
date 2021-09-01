2dfe2fc8493663c779932c63a58d19cb15807913
Alejandro Toro Daza <skullzo@outlook.com>
2021-09-01 10:04:00 -0500

    Source Code

--- src/main/java/org/ada/school/dto/UserDto.java
+++ src/main/java/org/ada/school/dto/UserDto.java
@@ -8,8 +8,11 @@ public class UserDto
 
     String lastName;
 
-    public UserDto()
+    public UserDto(String name, String email, String lastName)
     {
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
     }
 
     public String getName()
@@ -17,13 +20,19 @@ public class UserDto
         return name;
     }
 
+    public void setName(String name) { this.name = name; }
+
     public String getEmail()
     {
         return email;
     }
 
+    public void setEmail(String email) { this.email = email; }
+
     public String getLastName()
     {
         return lastName;
     }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
 }
